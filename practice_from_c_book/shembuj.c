d8b7a107282e7e3cf93c276ce71558c29965638b
Arsela Bita <arselabita>
2025-09-20 22:31:58 +0200

    example

---
+++ practice_from_c_book/shembuj.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* count characters in input; 1st version */
+
+int main(void)
+{
+    int c, nl;
+
+    nl = 0;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+            ++nl;
+    }
+    printf("%d\n", nl);
+}
