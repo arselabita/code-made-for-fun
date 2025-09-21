b801f27df43f63bfc4319858141043ddf0681e2a
Arsela Bita <arselabita>
2025-09-21 22:13:31 +0200

    getting the length of each argument

---
+++ clang/str_len.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+/*
+    Exercise 0.2. Write a program that prints each argument along with its length.
+    
+    Example:./strlen foo bar "hello world"
+    Output:
+            foo -> 3
+            bar -> 3
+            hello world -> 11
+*/
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    if (ac < 2)
+        return(0);
+    i = 1;
+    while (i < ac)
+    {
+        j = 0;
+        while (av[i][j])
+        {
+            
+        }
+        i++;
+    }
+    write(1, " ", 1);
+    return (0);
+}
