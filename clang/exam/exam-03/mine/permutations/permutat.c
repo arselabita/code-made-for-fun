2b734537a0926f2fdc731f852462086f6b3c2cbb
Arsela Bita <arselabita@gmail.com>
2025-10-08 16:00:05 +0200

    finished the exam

--- clang/exam/exam-03/mine/permutations/permutat.c
+++ clang/exam/exam-03/mine/permutations/permutat.c
@@ -1,34 +1,57 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void swap(char *a, char *b)
+void sorted(char *s)
 {
     char temp;
-
-    temp = *a;
-    *a = *b;
-    *b = temp;
+    for (int i = 0; s[i]; i++)
+    {
+        for (int j = i + 1; s[j]; j++)
+        {
+            if (s[i] > s[j])
+            {
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
 }
 
-void permute(char *a, int curr_index, int len)
+void perm(char *str, char *result, int *used, int dep, int len)
 {
-    if (curr_index == len - 1)
+    if (dep == len)
     {
-        printf("%s\n", a);
+        for (int i = 0; i < len; i++)
+            write(1, &result[i], 1);
+        write(1, "\n", 1);
         return ;
     }
-    for(int i = curr_index; i < len; i++)
+    for(int i = 0; i < len; i++)
     {
-        swap(&a[i], &a[curr_index]);
-        permute(a, curr_index + 1, len);
-        swap(&a[i], &a[curr_index]);
+        if (used[i])
+            continue ;
+        used[i] = 1;
+        result[dep] = str[i];
+        perm(str, result, used, dep + 1, len);
+        used[i] = 0;
     }
 }
+
 
 int main(int ac, char **av)
 {
     if (ac != 2)
-        return (1);
-    permute(av[1], 0, strlen(av[1]));
+        return 1;
+    int i = 0;
+    while (av[1][i])
+        i++;
+    int len = i;
+    char *result = malloc(len + 1);
+    int *used = calloc(len, sizeof(int));
+    sorted(av[1]);
+    perm(av[1], result, used, 0, len);
+    free(result);
+    free(used);
     return (0);
 }
